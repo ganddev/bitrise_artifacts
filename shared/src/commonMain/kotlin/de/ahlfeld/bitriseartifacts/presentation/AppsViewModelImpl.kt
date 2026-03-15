dd22d39cf5ad6082b2c01622ce5bb938646f28df
Bjoern Ahlfeld <3842688+ganddev@users.noreply.github.com>
2026-03-15 18:51:12 +0100

    Feature: Add AppsViewModelImplementation.

--- shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/presentation/AppsViewModelImpl.kt
+++ shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/presentation/AppsViewModelImpl.kt
@@ -1,2 +1,20 @@
-package de.ahlfeld.bitriseartifacts.presentation 
+package de.ahlfeld.bitriseartifacts.presentation
 
+import androidx.lifecycle.viewModelScope
+import de.ahlfeld.bitriseartifacts.extensions.stateInWhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+
+
+class AppsViewModelImpl : AppsViewModel() {
+    override val uiState: StateFlow<AppsUiState> = flow {
+        emit(AppsUiState.Loading)
+    }.stateInWhileSubscribed(
+        scope = viewModelScope,
+        initialValue = AppsUiState.Loading
+    )
+
+    override fun handleUiEvent(event: AppsUiEvent) {
+        TODO("Not yet implemented")
+    }
+}

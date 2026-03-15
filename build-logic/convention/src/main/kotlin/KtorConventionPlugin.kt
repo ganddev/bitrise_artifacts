9ab44d4e954d78e4684a0e37fa77e7561d5e52eb
Bjoern Ahlfeld <3842688+ganddev@users.noreply.github.com>
2026-03-15 21:21:21 +0100

    Feature: Move parts to convention plugins.

--- build-logic/convention/src/main/kotlin/KtorConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KtorConventionPlugin.kt
@@ -2,6 +2,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
@@ -11,10 +13,16 @@ class KtorConventionPlugin : Plugin<Project> {
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
             extensions.configure<KotlinMultiplatformExtension> {
+                sourceSets.getByName("androidMain").dependencies {
+                    implementation(libs.findLibrary("ktor-client-okhttp").get())
+                }
                 sourceSets.getByName("commonMain").dependencies {
                     api(libs.findLibrary("ktor-client-core").get())
                     implementation(libs.findLibrary("ktor-client-content-negotiation").get())
                     implementation(libs.findLibrary("ktor-serialization-json").get())
+                }
+                sourceSets.get("commonTest").dependencies {
+                    implementation(libs.findLibrary("ktor-client-mock").get())
                 }
             }
         }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKmpLibraryPluging)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.bitrise.kmp.network)
+    alias(libs.plugins.bitrise.kmp.compose)
 }
 
 kotlin {
@@ -38,29 +38,11 @@ kotlin {
     }
 
     sourceSets {
-        androidMain.dependencies {
-            implementation(libs.compose.uiToolingPreview)
-            implementation(libs.ktor.client.okhttp)
-        }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
-            implementation(libs.ktor.client.mock)
             implementation(libs.kotlinx.coroutines.test)
         }
         commonMain.dependencies {
-            implementation(libs.compose.runtime)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.ui)
-            implementation(libs.compose.components.resources)
-            implementation(libs.compose.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
-
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.serialization.json)
-            
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.kotlinx.coroutines.core)

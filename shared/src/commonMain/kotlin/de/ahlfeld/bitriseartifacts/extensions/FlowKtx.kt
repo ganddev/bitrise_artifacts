8eecc8eb4b646ab56999bdddb356658d3d7401de
Bjoern Ahlfeld <3842688+ganddev@users.noreply.github.com>
2026-03-15 18:49:52 +0100

    Feature: Add Flow extension.

--- shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/extensions/FlowKtx.kt
+++ shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/extensions/FlowKtx.kt
@@ -1,2 +1,21 @@
 package de.ahlfeld.bitriseartifacts.extensions
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+private const val ANR_TIMEOUT = 5_000L
+
+fun <T> Flow<T>.stateInWhileSubscribed(
+    scope : CoroutineScope,
+    initialValue : T,
+    stopTimeoutMillis: Long = ANR_TIMEOUT
+) : StateFlow<T> {
+    return stateIn(
+        scope = scope,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis),
+        initialValue = initialValue,
+    )
+}

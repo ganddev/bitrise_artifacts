086d5506682e6f76ff3cdff2d34d461c2971f397
Bjoern Ahlfeld <3842688+ganddev@users.noreply.github.com>
2026-03-15 18:50:58 +0100

    Feature: Add AppsUiState.

--- shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/presentation/AppsUiState.kt
+++ shared/src/commonMain/kotlin/de/ahlfeld/bitriseartifacts/presentation/AppsUiState.kt
@@ -1,2 +1,15 @@
-package de.ahlfeld.bitriseartifacts.presentation 
+package de.ahlfeld.bitriseartifacts.presentation
 
+sealed interface AppsUiState {
+    data object Loading : AppsUiState
+    data class Content(
+        val apps : List<AppItem>
+    )
+}
+
+
+data class AppItem(
+    val avatarUrl : String,
+    val ownerName: String,
+    val title : String,
+)
